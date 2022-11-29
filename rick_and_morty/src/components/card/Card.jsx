c7a12f8d810d9a9d3023e63d3522e8573554e288
Dac1977 <diegodac77@gmail.com>
2022-11-29 17:11:06 -0300

    Noveno commit

---
+++ rick_and_morty/src/components/card/Card.jsx
@@ -0,0 +1,13 @@
+import estilo from "./Card.module.css"
+
+export default function Card(props) {
+   return (
+      <div className={estilo.card}  >
+            <button onClick={props.onClose}>X</button>
+            <h2>{props.name}</h2>
+            <h2>{props.species}</h2>
+            <h2>{props.gender}</h2>
+            <img  src={props.image} alt={props.name} />
+      </div>
+   );
+}
